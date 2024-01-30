590263b661a86fc326f01f3d9a7eebdbcf4a630a
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-01-31 00:32:33 +0530

    feat: :sparkles: add base font theme

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+	return (
+		<div className="w-full min-h-screen flex bg-white flex-col items-center justify-center text-black font-public-sans text-2xl font-semibold tracking-wider">
+			Hi, Welcome Back
+		</div>
+	);
+};
+
+export default LoginPage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,10 @@ const config = {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
 			},
+			fontFamily: {
+				"public-sans": "'Public Sans', sans-serif",
+				barlow: "'Barlow', sans-serif",
+			},
 		},
 	},
 	plugins: [require("tailwindcss-animate")],
