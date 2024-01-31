1ee39fda5b42cd8d4c59d00fdf444a08dd0c7c6f
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-01-31 20:42:25 +0530

    feat: :sparkles: add login container

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,9 @@
+import { LoginContainer } from "@/components/login-page";
+
 const LoginPage = () => {
 	return (
-		<div className="w-full min-h-screen flex bg-white flex-col items-center justify-center text-black font-public-sans text-2xl font-semibold tracking-wider">
-			Hi, Welcome Back
+		<div className="w-full min-h-screen flex bg-white flex-col items-center justify-center text-black font-public-sans tracking-wider">
+			<LoginContainer />
 		</div>
 	);
 };
---
+++ components/login-page/login-container/login-container.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const LoginContainer = () => {
+	return (
+		<div className="w-full sm:max-w-[450px] flex flex-col border border-black rounded-md px-3 py-4">
+			<h1 className="text-2xl font-semibold">Sign in to NCA CRM</h1>
+		</div>
+	);
+};
+
+export default LoginContainer;
