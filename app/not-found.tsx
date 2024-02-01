a22f928144b3544a250e7cd030ed0fe8ce00b109
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-02-01 14:21:12 +0530

    feat: :sparkles: add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,30 @@
+import { AuthNavbar } from "@/components/common";
+import { SolidBtn } from "@/components/elements";
+
+const NotFoundPage = () => {
+	return (
+		<>
+			<AuthNavbar />
+			<main className="w-full min-h-screen flex bg-gradient-to-r from-light-background-natural to-light-background-default flex-col items-center justify-center font-public-sans px-4 pt-8">
+				<h1 className="text-3xl text-light-primary-text font-semibold">
+					Sorry, page not found!
+				</h1>
+
+				<p className="w-full max-w-[450px] text-center mt-5 text-light-disabled-text font-light">
+					{
+						"Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be sure to check your spelling."
+					}
+				</p>
+
+				<SolidBtn
+					title="Go to Home"
+					className="w-fit px-5 py-3 mt-5 bg-primary-main hover:bg-primary-main/90"
+					link="/"
+					type="internal"
+				/>
+			</main>
+		</>
+	);
+};
+
+export default NotFoundPage;
