0f3c2b44b91c7c2a27cd88514ed6c64c64f108f6
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-02-03 10:47:44 +0530

    feat: :sparkles: add main navbar hook

--- components/common/main-navbar/main-navbar.tsx
+++ components/common/main-navbar/main-navbar.tsx
@@ -5,14 +5,18 @@ import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/assets/images/logo.png";
 import { SolidBtn } from "@/components/elements";
+import { useMainNavbar } from "./hook";
 
 const MainNavbar = () => {
+	const { toggleSidebar } = useMainNavbar();
+
 	return (
-		<div className="w-full px-4 pt-3 pb-3 flex fixed top-0 left-0 bg-light-background-natural items-center justify-between">
+		<div className="w-full px-4 pt-3 pb-3 flex fixed top-0 left-0 bg-light-background-natural items-center justify-between z-30">
 			<div className="flex items-center">
 				<button
 					className="flex items-center justify-center mr-3"
 					aria-label="drawer button"
+					onClick={toggleSidebar}
 				>
 					<Menu size={22} className="text-light-primary-text" />
 				</button>
