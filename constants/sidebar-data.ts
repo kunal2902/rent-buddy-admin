fcc7f460ae79103cb5f328b7af0d76f86625a777
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-02-05 23:54:26 +0530

    feat: :sparkles: add items for sidebar

--- constants/sidebar-data.ts
+++ constants/sidebar-data.ts
@@ -1,5 +1,12 @@
-import { BarChart3, Warehouse } from "lucide-react";
-
+import {
+	BarChart3,
+	ShieldCheck,
+	Users,
+	Warehouse,
+	Receipt,
+	Percent,
+	Clipboard,
+} from "lucide-react";
 export const MainSidebarItems = [
 	{
 		id: 1,
@@ -12,5 +19,35 @@ export const MainSidebarItems = [
 		title: "Inventory",
 		link: "/inventory",
 		Icon: Warehouse,
+	},
+	{
+		id: 3,
+		title: "Customers",
+		link: "/customers",
+		Icon: Users,
+	},
+	{
+		id: 4,
+		title: "Users",
+		link: "/users",
+		Icon: ShieldCheck,
+	},
+	{
+		id: 5,
+		title: "Receipts",
+		link: "/receipts",
+		Icon: Receipt,
+	},
+	{
+		id: 6,
+		title: "Taxes",
+		link: "/taxes",
+		Icon: Percent,
+	},
+	{
+		id: 7,
+		title: "Reports",
+		link: "/reports",
+		Icon: Clipboard,
 	},
 ];
