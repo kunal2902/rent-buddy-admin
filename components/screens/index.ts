0a713ebbb97c9c15e8127b56cc549b540a32d020
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-03-09 16:21:36 +0530

    feat: :sparkles: add sub categories screen

--- components/screens/index.ts
+++ components/screens/index.ts
@@ -4,3 +4,4 @@ export * from "./items-screen/index";
 export * from "./settings-screen/index";
 export * from "./customers-screen/index";
 export * from "./categories-screen/index";
+export * from "./sub-categories-screen/index";
