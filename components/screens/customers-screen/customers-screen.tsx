e479b4851be7cc07c4ee660e8f2fddf6d6e53807
ayush3160 <kshitij3160@gmail.com>
2024-03-16 10:35:03 +0530

    Added Table

--- components/screens/customers-screen/customers-screen.tsx
+++ components/screens/customers-screen/customers-screen.tsx
@@ -2,9 +2,41 @@
 
 import { DashboardPageHeader } from "@/components/common";
 import { useCustomersScreen } from "./hook";
+import { TableComponent } from "@/components/common/table";
+import { TableColumn, TableRow } from "@/components/common/table/table";
 
 const CustomersScreen = () => {
 	const { isSidebarOpen } = useCustomersScreen();
+
+	const rows : TableRow[] = [
+		{
+			title : "Invoice"
+		},
+		{
+			title : "Status"
+		},
+		{
+			title : "Method"
+		},
+		{
+			title : "Amount"
+		},
+	]
+
+	const columns : TableColumn[][] = [[
+		{
+			content : "INV001"
+		},
+		{
+			content : "Paid"
+		},
+		{
+			content : "Credit Card"
+		},
+		{
+			content : "$250.00"
+		}
+	]]
 
 	return (
 		<main
@@ -16,6 +48,9 @@ const CustomersScreen = () => {
 				heading="Customers"
 				className="sm:pl-5 pl-3 pr-3 my-4 sm:text-2xl text-xl"
 			/>
+			<div className="box mx-10 p-4">
+				<TableComponent rows={rows} columns={columns} caption="A list of your recent invoices."/>
+			</div>
 		</main>
 	);
 };
