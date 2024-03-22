573d737640a6ec5f35bbdda7c434985c265d37af
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-03-22 16:51:21 +0530

    feat: :sparkles: add custom modal

--- components/screens/customers-screen/hook.ts
+++ components/screens/customers-screen/hook.ts
@@ -1,10 +1,15 @@
 import { sidebarStateAtom } from "@/atoms";
+import { toggleBooleanState } from "@/utils/toggle-boolean-state";
+import { useState } from "react";
 import { useRecoilValue } from "recoil";
 
 export const useCustomersScreen = () => {
 	const isSidebarOpen = useRecoilValue<boolean>(sidebarStateAtom);
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
 	return {
 		isSidebarOpen,
+		isModalOpen,
+		toggleModalState: toggleBooleanState(setIsModalOpen),
 	};
 };
---
+++ types/events/pointer-down-outside-event.ts
@@ -0,0 +1,3 @@
+export type PointerDownOutSideEvent = CustomEvent<{
+	originalEvent: PointerEvent;
+}>;
