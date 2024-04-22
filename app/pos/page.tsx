b79330ff10283610eb4c196f3b4f4502492e8d77
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-04-23 03:34:55 +0530

    feat: :sparkles: add pos navbar

--- app/pos/page.tsx
+++ app/pos/page.tsx
@@ -1,8 +1,15 @@
+import { PosNavbar } from "@/components/common";
+
 const POSHome = () => {
 	return (
-		<main className="w-full min-h-screen light-background-natural flex-col items-center justify-center font-public-sans px-4 pt-8">
-			<h1 className="text-light-primary-text">Hello from POS Screen</h1>
-		</main>
+		<>
+			<PosNavbar />
+			<main className="w-full min-h-screen light-background-natural flex-col items-center justify-center font-public-sans px-4 pt-14 bg-light-background-natural">
+				<h1 className="text-light-primary-text">
+					Hello from POS Screen
+				</h1>
+			</main>
+		</>
 	);
 };
 
