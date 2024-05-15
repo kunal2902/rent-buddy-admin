5b97beb03d4ef0f28768cb5c90e805bd3505c9ef
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-05-16 00:58:07 +0530

    feat: :sparkles: fix margin right issues

--- containers/5_sub_categories/sub_categories_container.tsx
+++ containers/5_sub_categories/sub_categories_container.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { Plus } from 'lucide-react';
-import { useEffect, useState } from 'react';
-import { useSubCategoriesContainer } from './hook';
-import { DashboardPageHeader } from '@/components';
-import { SubCategoryModel } from '@/models';
-import { getSubCategoryApi } from '@/utils';
-import CreateSubCategoryModal from './add_sub_category_modal';
+import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useSubCategoriesContainer } from "./hook";
+import { DashboardPageHeader } from "@/components";
+import { SubCategoryModel } from "@/models";
+import { getSubCategoryApi } from "@/utils";
+import CreateSubCategoryModal from "./add_sub_category_modal";
 
 const SubCategoriesContainer = () => {
 	const {
@@ -15,26 +15,32 @@ const SubCategoriesContainer = () => {
 		toggleCreateSubCategoryModalOpen,
 	} = useSubCategoriesContainer();
 
-	const [subCategoryList, setSubCategoryList] = useState<SubCategoryModel[]>([]);
+	const [subCategoryList, setSubCategoryList] = useState<SubCategoryModel[]>(
+		[],
+	);
 	const [callApi, setCallApi] = useState(true);
 
 	useEffect(() => {
 		if (callApi) {
-			getSubCategoryApi((data: any) => {
-				setSubCategoryList(data);
-				setCallApi(false);
-			}, () => {
-				setCallApi(false);
-			}, () => {
-				setCallApi(false);
-			}).then();
+			getSubCategoryApi(
+				(data: any) => {
+					setSubCategoryList(data);
+					setCallApi(false);
+				},
+				() => {
+					setCallApi(false);
+				},
+				() => {
+					setCallApi(false);
+				},
+			).then();
 		}
 	}, [callApi]);
 
 	return (
 		<main
 			className={`flex min-h-screen w-full bg-light-background-natural flex-col pt-14 ${
-				isSidebarOpen ? 'lg:pl-64 pl-0' : 'pl-16'
+				isSidebarOpen ? "lg:pl-64 pl-0" : "pl-16"
 			}`}
 		>
 			<DashboardPageHeader
@@ -42,11 +48,11 @@ const SubCategoriesContainer = () => {
 				className="sm:pl-5 pl-3 pr-3 my-4 sm:text-2xl text-xl"
 				button
 				buttonProps={{
-					title: 'New Sub Category',
-					titleClassName: 'sm:flex hidden',
+					title: "New Sub Category",
+					titleClassName: "sm:flex hidden",
 					onClick: toggleCreateSubCategoryModalOpen,
-					className: 'rounded-md w-fit text-grey-100 text-sm',
-					children: <Plus size={20} className="sm:mr-2 mr-0" />,
+					className: "rounded-md w-fit text-grey-100 text-sm",
+					children: <Plus size={20} className="mr-0" />,
 				}}
 			/>
 
