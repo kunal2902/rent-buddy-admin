92a55e08632b0926f3f89716474d50313cf00914
Lalit Fauzdar <lalit.fauzdar1@gmail.com>
2024-05-28 13:24:10 +0530

    ImageUrl fix in config.ts. Created Responsive loading_overlay_component.tsx and no_data_found.tsx components. Removed unnecessary hooks. New UI done in all the screens. Fixed loading and search loading not visible in all the screens. Implemented Add Sub Category modal.
    Implemented new UI in ActivityLogs, Customer, Reports, Roles and Users.
    UI fixes in TagsContainer, POS Cart section, Item types container and Dashboard container.
    Fixed mantine background color issue.
    Fixed import error of AddUserModal in pos_cart_section.tsx.
    Deleted unnecessary hook files.
    
    Resolved Merge error on Sanjay's branch in tags_container.tsx.
    Resolved ESLint issues.
    
    Completed Customers, Reports, Users and Roles section.
    Improved Items and Tags section.
    Deleted unnecessary hooks.
    Made global sidebar component.
    Added Add Role modal, user modal.
    Created loading_overlay_component.tsx, main_component.tsx, pagination_component.tsx
    
    Made the Item/Tags Header a global component.
    Exported Theme configs.
    Created ColoredInputTheme, CenteredInputTheme.
    
    Fixed ESLint LF problem.
    Fixed ESLint Problems.
    
    Dark mode implemented, colors defined, implemented in main_navbar.tsx.
    Implemented New Header in items_container.tsx.
    Improved the header design in tags_container.tsx.
    Made sort_items and select_items global in constants.
    
    Merging into Development for deployment.
    
    Added PopConfirm for Disabling and Delete Action.
    Improved the Table Design.
    Improved the Add Tag dialog.
    Edit Tag implemented.
    Added box_component.tsx, paper_component.tsx and pop_confirm.tsx.
    
    Changed Environment to Dev in Main branch.
    
    Removed unnecessary hooks.
    Fixed errors related to TextInput's onChange.
    Implemented setValue in text_input_component.tsx, select_component.tsx and password_input_component.tsx.
    Removed unnecessary functions related to onChange and onSelect.
    
    Added .env.
    Removed .env files from .gitignore.
    
    Improved the layout of Tags Container.
    Improved the Header and Table area of Tags Container.
    Added SortButtonComponent.
    Added TitleComponent.
    
    Pos design complete.
    Pending - Make ScrollArea's height dynamic and Implement the APIs.
    
    Fixed Width issue in the sidebar.
    Improved the layout of the pos.
    Changed the unused var as warning from error in .eslintrc.
    
    Added Image, Chip, Card and Number Input component.
    Fixed Comments of respective components.
    Minor changes in the POS layout.
    
    Changed to double Quote
    Added Scrollbar in Sidemenu.
    Fixed SideNav width issue and icon size.
    
    Pos design partially complete.
    Added Group component.
    Removed unnecessary files.
    
    Fixed Tag API error callback error
    Completed Sidebar with NavLink component with custom background colors, selection, tooltip hover issue fix, matching icons etc.
    Manually merged Sanjay's branch changes into network_utils.ts, config.ts and .eslintrc.
    
    Corrected back ESLint's. single quote.
    Added multiple new components.
    Fixed header design.
    Minor fixes in Navbar design.
    Added constants for routes.
    Added Space component instead of relying on margins.
    
    Fixed yarn issue.
    Fixed TailwindCSS Error.
    Changed the folder structure to support NextJS's app router.
    Merged Changed from Sanjay's branch to implement newly created Models, APIs and their implementation.
    Added missing Select and Table component.
    Implemented the new components and resolved the errors.
    All the conversion to Mantine from ShadCN is complete.
    
    Added Avatar for MainNavBar.
    Changed color theme in tailwind.config.ts.
    Added User data cookie storage for Login API.
    Added Style props for Solid button.
    Enhanced the design of the Main Navbar.
    Added constants in config.ts for Colors, String and API constants.

--- utils/config.ts
+++ utils/config.ts
@@ -72,7 +72,8 @@ export const apiUrl: string = process.env.NODE_ENV === "production" ||
 isProduction ?
 	"https://nca-crm-api-peggi.ondigitalocean.app/api/v1" :
 	"http://localhost:8000/api/v1";
-export const imageUrl: string = process.env.NODE_ENV === "production" ?
+export const imageUrl: string = process.env.NODE_ENV === "production" ||
+isProduction ?
   "https://nca-crm-api-peggi.ondigitalocean.app/image" :
   "http://localhost:8000/image";
 export const loginAPIPath: string = `${apiUrl}/auth/login`;
